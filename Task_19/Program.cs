fac917df4dd99e909a206e1d209a8d6ff4808b91
Alexandr <guk.zverolis@yandex.ru>
2022-10-23 15:58:10 +0300

    result commit

--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -4,35 +4,42 @@
 12821 -> да
 23432 -> да
 */
+int number = ReadInt("Введите пятизначное число: ");
+
+if (number == 0)
+{
+    Console.Write("Введён 0.");
+} 
+else 
+{
+    if (number < -99999 || number > 99999)
+    {
+    Console.Write("Введено неподходящее число!");
+    }
+    else
+    {
+    Calc(number);
+    }
+}
+
 int ReadInt(string message)
 {
     Console.Write(message);
     return Convert.ToInt32(Console.ReadLine());
 }
 
-int number = ReadInt("Введите пятизначное число: ");
-
-if (number < 0)
+void Calc(int number)
 {
-    number = number * -1;
-} 
-if (number <= 9999 || number > 99999)
-{
-    Console.Write("Введено неподходящее число");
-}
-else
-{
-   int num1 = number / 10000;
-   int num2 = number / 1000 % 10;
-   int num4 = number % 100 / 10;
-   int num5 = number % 10;
-
-   if (num1 == num5 && num2 == num4)
-   {
-    Console.WriteLine("Число палиндром");
-   }
-   else 
-   {
-    Console.WriteLine("Число не палиндром");
-   }
+        int num1 = number / 10000;
+        int num2 = number / 1000 % 10;
+        int num4 = number % 100 / 10;
+        int num5 = number % 10;
+        if (num1 == num5 && num2 == num4)
+        {
+            Console.WriteLine("Палиндром.");
+        }
+        else 
+        {
+            Console.WriteLine("Не палиндром.");
+        }
 }
--- Task_23/Program.cs
+++ Task_23/Program.cs
@@ -3,23 +3,45 @@
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125
 */
+
+int number = ReadInt("Введите число: ");
+if (number == 0)
+{
+    Console.WriteLine("0");
+}
+else if (number < 0)
+    {
+    DegNegativ(number);
+    }
+    else 
+    {
+    DegPositiv(number);
+    }
+
 int ReadInt(string message)
 {
     Console.Write(message);
     return Convert.ToInt32(Console.ReadLine());
 }
- int num = ReadInt("Введите число: ");
- int count = 1;
-if (num < 0)
+
+void DegNegativ(int number)
 {
-    Console.Write("Введите положительное число");
+    int count = -1;
+    while (count > number)
+    {    
+    Console.Write(count * count * count + ", ");
+    count = count + -1;
+    }
+Console.Write(count * count * count + ".");
 }
-else 
+
+void DegPositiv(int number)
 {
- while (count < num)
- {    
+    int count = 1;
+    while (count < number)
+    {    
     Console.Write(count * count * count + ", ");
     count++;
- }
+    }
 Console.Write(count * count * count + ".");
 }
